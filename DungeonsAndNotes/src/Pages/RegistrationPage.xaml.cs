dab1ee6a009bea27146cb64983c2dfbbb904183a
antoniosos <67897276+antoniosos@users.noreply.github.com>
2024-06-16 17:44:37 +0200

    a

--- DungeonsAndNotes/src/Pages/RegistrationPage.xaml.cs
+++ DungeonsAndNotes/src/Pages/RegistrationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Net.Http;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace DungeonsAndNotes.Pages;
@@ -10,29 +11,76 @@ public partial class RegistrationPage : Page
         InitializeComponent();
     }
 
-    private void Register_Click(object sender, RoutedEventArgs e)
+    private async void Register_Click(object sender, RoutedEventArgs e)
+{
+    if (Validator.ValidateUserName(TxtUsername.Text) != Validator.UserNameValidity.IsValid ||
+           Validator.ValidatePassword(TxtPassword.Password, TxtConfirmPassword.Password) != Validator.PasswordValidity.IsValid)
     {
-        while (Validator.ValidateUserName(TxtUsername.Text) != Validator.UserNameValidity.IsValid ||
-               Validator.ValidatePassword(TxtPassword.Password, TxtConfirmPassword.Password) != Validator.PasswordValidity.IsValid)
-        {
-            // 
-        }
-        
-        
-        
-     
-        
-        
-        throw new NotImplementedException();
+        Validator.UserNameValidity userNameValidity = Validator.ValidateUserName(TxtUsername.Text);
+        ShowNameWarning(userNameValidity);
+
+        Validator.PasswordValidity passwordValidity = Validator.ValidatePassword(TxtPassword.Password);
+        ShowPasswordWarning(passwordValidity);
+
+        return;
     }
+
+    try
+    {
+        var registrationInfo = new Dictionary<string, string>()
+        {
+            { "username", TxtUsername.Text },
+            { "password", TxtPassword.Password }
+        };
+
+        // Execute HTTP request asynchronously on a background thread
+        HttpResponseMessage response = await Task.Run(async () =>
+        {
+            var content = new FormUrlEncodedContent(registrationInfo);
+            return await Connection.GetConnection().Client.PostAsync("http://127.0.0.1:80/register", content);
+        });
+
+        // Ensure the response was successful before proceeding
+        response.EnsureSuccessStatusCode();
+
+        // Read and display the response content
+        string responseContent = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseContent);
+    }
+    catch (Exception ex)
+    {
+        // Handle any exceptions
+        Console.WriteLine("Exception: " + ex.Message);
+    }
+}
+
 
     private void ShowNameWarning(Validator.UserNameValidity validity)
     {
-        
+        switch (validity)
+        {
+            case Validator.UserNameValidity.TooShort:
+                UserNameWarning.Content = "Too short!";
+                break;
+            case Validator.UserNameValidity.TooLong:
+                UserNameWarning.Content = "Too long!";
+                break;
+            case Validator.UserNameValidity.Exists:
+                UserNameWarning.Content = "Already exists!";
+                break;
+        }
     }
 
-    private void ShowPasswordWarning()
+    private void ShowPasswordWarning(Validator.PasswordValidity validity)
     {
-        
+        switch (validity)
+        {
+            case Validator.PasswordValidity.TooShort:
+                PasswordNameWarning.Content = "Too short!";
+                break;
+            case Validator.PasswordValidity.DontMatch:
+                PasswordNameWarning.Content = "Password don't match!";
+                break;
+        }   
     }
 }
--- DungeonsAndNotes/src/Validator.cs
+++ DungeonsAndNotes/src/Validator.cs
@@ -1,4 +1,8 @@
-﻿namespace DungeonsAndNotes;
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace DungeonsAndNotes;
 
 public static class Validator
 {
@@ -14,10 +18,6 @@ public static class Validator
     }
     public static UserNameValidity ValidateUserName(string name)
     {
-        if (IsNameExistent(name))
-        {
-            return UserNameValidity.Exists;
-        }
         if(IsNameTooLong(name))
         {
             return UserNameValidity.TooLong;
@@ -26,14 +26,38 @@ public static class Validator
         {
             return UserNameValidity.TooShort;
         }
+        if (IsNameExistent(name).Result)
+        {
+            return UserNameValidity.Exists;
+        }
 
         return UserNameValidity.IsValid;
     }
 
-    private static bool IsNameExistent(string name)
+    private static async Task<bool> IsNameExistent(string name)
     {
-        // tady to pošle GET request na server který checke jestli už takové jméno v databázi je
-        throw new NotImplementedException();
+        // tady to pošle POST request na server který checke jestli už takové jméno v databázi je
+        
+        var registertationInfo = new Dictionary<string, string>()
+        {
+            {
+                "username", name
+            },
+        };
+        
+        var jsonString = JsonSerializer.Serialize(registertationInfo);
+        StringContent stringContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+        
+        var response = await Connection.GetConnection().Client.PostAsync("http://127.0.0.1:80/exists/name", stringContent);
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+
+        Console.WriteLine(response);
+        return false;
+
     }
 
     private static bool IsNameTooLong(string name)
